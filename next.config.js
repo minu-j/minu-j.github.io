093669a743bcd2abc5aa3938fa043376face9cd0
minu-j <minu.j.dev@gmail.com>
2023-07-24 01:10:06 +0900

    fix: image unoptimize

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = { images: { unoptimized: true } };
 
-module.exports = {
-  experimental: {},
-};
+module.exports = nextConfig;
