868953b9ec63f6336f1145cb16156ec510d9280a
minu-j <minu.j.dev@gmail.com>
2023-07-28 02:03:40 +0900

    add project image

--- src/components/Home/Projects/ProjectCard.tsx
+++ src/components/Home/Projects/ProjectCard.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import styles from "./ProjectCard.module.scss";
 import Card from "../../common/Card/Card";
 import { SiFigma, SiGithub } from "react-icons/si";
+import { IconType } from "react-icons";
 
 interface projectCardProps {
   data: {
     title: string;
     date: string;
+    skills: Array<IconType>;
+    image: string;
     content: string;
+    github: string;
+    figma: string;
   };
 }
 
@@ -15,17 +20,30 @@ export default function ProjectCard({ data }: projectCardProps) {
   return (
     <section className={styles.project}>
       <Card width="100%" height="220px">
-        <div className={styles.date}>{data.date}</div>
+        <div
+          style={{
+            backgroundImage: `url(https://drive.google.com/uc?id=${data.image})`,
+          }}
+          className={styles.cardcontents}
+        >
+          <p className={styles.date}>{data.date}</p>
+          <div className={styles.skills}>
+            {data.skills.map((value, idx) => {
+              const Icon = value;
+              return <Icon key={`icon-${idx}`} />;
+            })}
+          </div>
+        </div>
       </Card>
       <div className={styles.text}>
         <h2 className={styles.title}>{data.title}</h2>
         <p className={styles.content}>{data.content}</p>
         <div className={styles.link}>
-          <a>
+          <a onClick={() => window.open(data.github)}>
             <SiGithub />
             <span>GitHub</span>
           </a>
-          <a>
+          <a onClick={() => window.open(data.figma)}>
             <SiFigma />
             <span>Figma</span>
           </a>
--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -2,6 +2,23 @@ import React from "react";
 import styles from "./Projects.module.scss";
 import { BsArrowRightCircle } from "react-icons/bs";
 import ProjectCard from "./ProjectCard";
+import {
+  SiAndroidstudio,
+  SiDart,
+  SiDjango,
+  SiFigma,
+  SiFirebase,
+  SiFlutter,
+  SiGit,
+  SiJavascript,
+  SiPython,
+  SiReact,
+  SiRedux,
+  SiSass,
+  SiTypescript,
+  SiVuedotjs,
+  SiXcode,
+} from "react-icons/si";
 
 interface projectsProps {
   scrollPosition: number;
@@ -13,28 +30,52 @@ export default function Projects({ scrollPosition, screenVh }: projectsProps) {
     {
       title: "42",
       date: "2022.06.~2022.06.",
+      skills: [SiTypescript, SiReact, SiFirebase, SiGit, SiFigma],
+      image: "1YvAF0NMYV6g0QKPsPSR-X0HRPCjrsN00",
       content: `사이는 일상 속 내 주변을 스쳐 지나가는 인연들에 대한 호기심에서 시작되었습니다. 
       주변 생각을 피드에 기록하고, 우연히 다시 마주친 익명의 낯선 인연과의 만남을 
       알려주는 위치기반 익명 SNS 서비스입니다.`,
+      github: "https://naver.com",
+      figma: "https://google.com",
     },
     {
       title: "Co-cook!",
       date: "2022.06.~2022.06.",
+      skills: [SiDart, SiFlutter, SiXcode, SiAndroidstudio, SiGit, SiFigma],
+      image: "1r7oXn9D8QKJERO_1lbhUni3med0Jt9fi",
       content: `Co-cook!은 요리 중 스마트폰을 손으로 만지기 어렵다는 점에 착안한 서비스로
       AI기반 음성인식 기술로 음성 명령을 인식하여 레시피를 제어할 수 있는
       크로스 플랫폼 모바일 앱입니다.`,
+      github: "https://naver.com",
+      figma: "https://google.com",
     },
     {
       title: "마이 리틀 쥬라기",
       date: "2022.06.~2022.06.",
+      skills: [SiTypescript, SiReact, SiRedux, SiGit, SiFigma],
+      image: "1W71NuV1ZaKgHdAQQcUwN6yvMzzRKeUxo",
       content: `마이리틀 쥬라기는 최근 떠오르는 이색 반려동물 시장을 공략한
       전국 이색 반려동물 샵 통합 관리 서비스입니다.
       체험 이벤트 예약과 실시간 방송 중계 모니터링을 지원합니다.`,
+      github: "https://naver.com",
+      figma: "https://google.com",
     },
     {
       title: "Argos",
       date: "2022.06.~2022.06.",
+      skills: [
+        SiJavascript,
+        SiVuedotjs,
+        SiSass,
+        SiPython,
+        SiDjango,
+        SiGit,
+        SiFigma,
+      ],
+      image: "1A22COM8zn2mWMacCspvkW3qToRvX2Nb0",
       content: `ARGOS는 자체적인 알고리즘을 통해 사용자의 영화 평가 데이터를 기반으로 한 개인 맞춤형 영화 추천 서비스입니다.`,
+      github: "https://naver.com",
+      figma: "https://google.com",
     },
   ];
   return (
